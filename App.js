cb26cdf70821e2356c1f1f603b0f0e1f763ef2f2
jgutia <jgutierreza@ids.com.mx>
2020-01-21 18:49:14 -0600

    dividing by components

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { useState }  from 'react';
+import { StyleSheet, Text, View ,TextInput, Button, ScrollView, FlatList} from 'react-native';
+
+import GoalItem from './components/GoalItem';
+
+export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(courseGoals => [...courseGoals, {key: Math.random().toString(), value:enteredGoal}]);
+  }
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder="Add your goal" 
+          style={styles.input} 
+          onChangeText={goalInputHandler} 
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler}/>
+      </View>
+      <FlatList 
+        data={courseGoals}
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen:{
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent:'space-between',
+    alignItems: 'center'
+  },
+  input:{
+    width: '80%',
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  },
+});
